b9a5c290734f43fe09e470dcad6caa94c63eea95
Тынышбаев Алибек Алмасулы <@corp.itstep>
2019-06-19 19:46:49 +0600

    first commit

---
+++ Bank/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Program
+    {
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+
+            for (int i = 1; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(BankWork);
+                threads[i].Start();
+            }
+            Console.ReadLine();
+        }
+
+        static void BankWork()
+        {
+            User user = new User()
+            {
+                Money = 5000,
+                Name = "Нурсултан"
+            };
+            Console.WriteLine($"\n {user.Name} баланс: {user.Money}\n Банк ID: {Thread.CurrentThread.ManagedThreadId} начал работу");
+
+            for (int i = 0; i < 5; i++)
+            {
+                var randNumber = new Random().Next(0, 2);
+                var randMoney = new Random().Next(100, 1000);
+
+               
+                if (randNumber == 0)
+                {
+
+                    user.Money += randMoney;
+                    Console.WriteLine($" Пополнение {randMoney} На вашем счету: {user.Money}");
+                }
+                else if (randNumber == 1)
+                {
+
+                    user.Money -= randMoney;
+                    Console.WriteLine($" Снятие {randMoney} На вашем счету: {user.Money}");
+                }
+
+             
+            }
+        }
+    }
+
+}
+
---
+++ Bank/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Bank
+{
+    public class User
+    {
+        public int Money { get; set; }
+        public string Name { get; set; }
+    }
+}
